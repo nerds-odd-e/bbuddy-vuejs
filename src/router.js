3706acfd586052106f06a889c04af93c2a2b99a5
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-06-22 12:23:33 +0800

    add account: implement with view without action dispatch

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Add from './views/Add.vue'
 import About from './views/About.vue'
 
 Vue.use(Router)
@@ -11,6 +12,10 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/add',
+      component: Add
     },
     {
       path: '/about',
---
+++ src/views/Add.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="add-account">
+    <div>
+      <span>Name: </span>
+      <input type="text" v-model="name"/>
+    </div>
+    <div>
+      <span>Balance: </span>
+      <input type="text" v-model="balance"/>
+    </div>
+    <div>
+      <button v-on:click="addAccount">Add Account</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'add-account',
+  data: () => ({
+    name: '',
+    balance: ''
+  }),
+  methods: {
+    addAccount () {
+    }
+  }
+}
+</script>
