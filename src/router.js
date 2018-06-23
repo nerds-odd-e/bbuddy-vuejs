db6588fb398f4415f5b60cccea77fc8dedc3c60f
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-06-23 12:11:53 +0800

    remove unneeded code

--- src/router.js
+++ src/router.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Add from './views/Add.vue'
-import About from './views/About.vue'
 
 Vue.use(Router)
 
@@ -10,17 +9,11 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
       component: Home
     },
     {
       path: '/add',
       component: Add
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About
     }
   ]
 })
