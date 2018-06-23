fce97d7709c02fb16ec2ef294f0e0c8cc0a3a142
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-06-23 22:45:28 +0800

    refactoring: move host to config

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
+import { HOST } from '@/config'
 
 export const callApi = (endpoint, method, data) => {
   return axios.request({
-    url: 'http://localhost:3000/' + endpoint,
+    url: HOST + endpoint,
     headers: {
       'access-token': 'evpZgOmKhHDhvyGCWAnxmQ',
       'token-type': 'Bearer',
---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+const env = process.env.NODE_ENV === 'production' ? 'prod' : 'dev'
+
+const hosts = {
+  dev: 'http://192.168.1.10:3000/',
+  prod: 'https://prod.com'
+}
+
+export const HOST = hosts[env]
